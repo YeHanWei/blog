5c0c26dbd8362e9a1af9b72c2739748a25049447
叶汉伟 <1028207768@qq.com>
2019-03-18 22:50:26 +0800

    添加cookie操作方法到vue全局调用

--- src/client/adminPage/index.js
+++ src/client/adminPage/index.js
@@ -19,3 +19,44 @@ new Vue({
   }
 })
 
+/**
+ * 读取cookie
+ * @param name 读取的名字
+ * @return {string} 对应name的值
+ */
+Vue.prototype.getCookie = function (name) {
+  let cookieName = encodeURIComponent(name) + '='
+  let start = document.cookie.indexOf(cookieName)
+  let value = null
+  if (start > -1) { // 属性名存在
+    let end = document.cookie.indexOf(';', start)
+    if (end === -1) { // 最后一个属性
+      end = document.cookie.length
+    }
+    value = decodeURIComponent(document.cookie.substring(start + cookieName.length, end))
+  }
+  return value
+}
+
+Vue.prototype.setCookie = function (name, value, expireSeconds, path, domain, secure) {
+  let cookieText = encodeURIComponent(name) + '=' + encodeURIComponent(value)
+  if (expireSeconds) {
+    let _date = new Date()
+    _date.setSeconds(_data.getSeconds() + expireSeconds)
+    cookieText += '; expires=' + _date.toGMTString()
+  }
+  if (path) {
+    cookieText += '; path=' + path
+  }
+  if (domain) {
+    cookieText += '; domain=' + domain
+  }
+  if (secure) {
+    cookieText += '; secure'
+  }
+  document.cookie = cookieText
+}
+
+Vue.prototype.delCookie = function (name, path, domain, secure) {
+  this.setCookie(name, '', 0, path, domain, secure)
+}
